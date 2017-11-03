cdd8bc0359046f2a24991b8392de2703a7e26ddb
Samuel Cozannet <samuel.cozannet@madeden.com>
2017-11-03 13:21:29 +0100

    adding PR management

---
+++ git/create-pr/create_pr.rb
@@ -0,0 +1,34 @@
+#!/usr/bin/ruby
+
+require 'octokit'
+Octokit.access_token = ENV['GITHUB_ACCESS_TOKEN']
+
+repo_name = ENV['REPO_NAME'] || 'jtarchie/pullrequest-resource'
+base_branch = ENV['BASE_BRANCH'] || 'test-merge'
+test_branch = "test-merge-#{Time.now.to_i}"
+sha = Octokit.ref(repo_name, "heads/#{base_branch}").object.sha
+
+Octokit.create_ref(repo_name, "heads/#{test_branch}", sha)
+
+blob_sha = Octokit.contents(
+  repo_name,
+  path: 'README.md',
+  ref: test_branch
+).sha
+
+Octokit.update_contents(
+  repo_name,
+  'README.md',
+  'Updating content',
+  blob_sha,
+  "File content #{Time.now.to_s}",
+  branch: test_branch
+)
+
+Octokit.create_pull_request(
+  repo_name,
+  base_branch,
+  test_branch,
+  'testing the latest build',
+  'testing the latest build'
+)
