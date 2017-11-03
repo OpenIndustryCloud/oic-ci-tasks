64dfaa691e4ed4318c983f635782f19aa6ee08af
Samuel Cozannet <samuel.cozannet@madeden.com>
2017-11-03 09:36:48 +0100

    adding build step for go

---
+++ function/build/build_function.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# This script deploys a Fission Function in a cluster for CI analysis
+#
+# Variables: 
+#  - FUNCTION_NAME: name of the function to deploy. 
+#  - FUNCTION_ENVIRONMENT: name of the environment of the function: go, python, nodejs or binary
+#  - CODE_PATH: where to find the code of the function
+
+set -eux 
+
+DIR="$(echo ${CODE_PATH} rev | cut -f2- -d'/' | rev)"
+FUNCTION="$(echo ${CODE_PATH} rev | cut -f1 -d'/' | rev)"
+
+cd "${DIR}"
+
+go build -buildmode=plugin \
+  -o /built-artifacts/function.so \
+  "${FUNCTION}"
+
