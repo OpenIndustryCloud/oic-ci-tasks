72af405bda2ce133276a10de47923f5024918a7b
Samuel Cozannet <samuel.cozannet@madeden.com>
2017-11-03 10:29:49 +0100

    adding option to change the method to test functions

--- function/test/test_function.sh
+++ function/test/test_function.sh
@@ -2,11 +2,11 @@
 
 # This script will run a newman/postman query against an API
 cat "${COLLECTION}" | \
-	jq ".item[].request.url.raw = \"${PROTOCOL}://${FISSION_ROUTER}:${TARGET_PORT}/${ENVIRONMENT}/${FUNCTION_NAME}\" | \
+	jq ".item[].request.method = \"${FUNCTION_METHOD}\" | \
+		.item[].request.url.raw = \"${PROTOCOL}://${FISSION_ROUTER}:${TARGET_PORT}/${ENVIRONMENT}/${FUNCTION_NAME}\" | \
 		.item[].request.url.host = [ \"${FISSION_ROUTER}\" ] | \
 		.item[].request.url.path = [ \"${ENVIRONMENT}\", \"${FUNCTION_NAME}\" ] | \
-		.item[].request.url.port = \"${TARGET_PORT}\"" > /tmp/collection.json
+		.item[].request.url.port = \"${TARGET_PORT}\"" \
+		> /tmp/collection.json
 
 newman run /tmp/collection.json
-
-
