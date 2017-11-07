61e4ea4654f7a38e7676586da9f0c934fde51ba6
Samuel Cozannet <samnco@gmail.com>
2017-11-07 11:54:29 +0000

    moving infra to another repo

---
+++ api/publish/publish_function.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# This script deploys a Fission Function in a cluster for CI analysis
+#
+# Variables: 
+#  - ENVIRONMENT: dev, staging, production... Will result in prefixing the URL by its content
+#  - FUNCTION_NAME: name of the function to deploy. 
+#  - FUNCTION_ENVIRONMENT: name of the environment of the function: go, python, nodejs or binary
+#  - CODE_PATH: where to find the code of the function
+#  - FUNCTION_METHOD: GET, POST, DELETE or any other specific method needed
+
+set -eux 
+
+case "${FUNCTION_ENVIRONMENT}" in 
+	"go" )
+		CODE_ARG="--package"
+	;;
+	* )
+		CODE_ARG="--code"
+	;;
+esac
+
+fission function create \
+	--name ${ENVIRONMENT}-${FUNCTION_NAME} \
+	--env ${FUNCTION_ENVIRONMENT} \
+	${CODE_ARG} "${CODE_PATH}" \
+	|| { 
+		fission function update \
+			--name ${FUNCTION_NAME} \
+			${CODE_ARG} "${CODE_PATH}" 
+	}
+
+fission route create \
+	--method ${FUNCTION_METHOD} \
+	--url /${ENVIRONMENT}/${FUNCTION_NAME} \
+	--function ${ENVIRONMENT}-${FUNCTION_NAME} \
+	|| true
